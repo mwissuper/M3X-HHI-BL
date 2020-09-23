6737334acab66c10034be10b78546e0529e5cc45
M Wu <40603605+mwissuper@users.noreply.github.com>
2020-09-23 13:37:39 -0400

    Add files via upload

--- plot_group_data.m
+++ plot_group_data.m
@@ -10,9 +10,33 @@ for ind = 1:length(subj_array)
     data = load(filename);
     
     Fz1bias(ind) = data.biasFz1;
+     % Convert to N after filtering. Multiply by factor to
+     % convert to N from V. FT2 gains changed 12/5/19 from 5V
+     % FS to 10V FS.
+     if subj < 10
+         SFz2 = 1/0.01;
+     else
+         SFz2 = 1/0.02;
+     end
+     SFz1 = 1/0.02;
+     Fz1(ind) = data.biasFz1*SFz1; 
+%      Fz2(ind) = data.biasFz2*SFz2;
+                 
     xlab{ind} = num2str(subj);
 end
 
-plot(1:length(subj_array),Fz1bias,'x'),ylabel('Fz1 zero bias (V)');
+% Plot in volts
+plot(1:length(subj_array),Fz1bias,'x');
+ylabel('Fz1 zero bias (V)');
 box off, title('Solo walking trials as ref. for ea. participant');
 set(gca,'tickdir','out','xticklabel',xlab),xlabel('Participant');
+
+figure
+% Plot in N
+plot(1:length(subj_array),Fz1,'x');
+ylabel('Fz1 zero bias (N)');
+box off, title('Solo walking trials as ref. for ea. participant');
+set(gca,'tickdir','out','xticklabel',xlab),xlabel('Participant');
+
+% 1/4 of range of Fz drift across subj's is 1.43V. Use this for Fx or Fy in
+% beamwalking study
